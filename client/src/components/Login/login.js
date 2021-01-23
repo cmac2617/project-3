82d38ddbbd66ad141a9bd59e0a99044eb777f8cb
Andrew Novak <andrew.n.novak@gmail.com>
2021-01-23 11:21:02 -0500

    Update login.js
    added import information

--- client/src/components/Login/login.js
+++ client/src/components/Login/login.js
@@ -1,3 +1,8 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./login.css";
+
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
