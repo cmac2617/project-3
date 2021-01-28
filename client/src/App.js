36d927ad5416c8f5842eb225df4774fe5c745699
Andrew Novak <andrew.n.novak@gmail.com>
2021-01-27 21:16:26 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,30 @@
-
-import "./App.css"
-import Main from "./pages/Main.js"
-import Navbar from "./components/Navbar/Navbar.js"
-import register from "./pages/Register.js"
-import Login from "./components/Login/Login.js"
+import "./App.css";
+import Main from "./pages/main.js";
+import Navbar from "./components/Navbar/Navbar.js";
+import register from "./pages/register.js";
+import Login from "./components/Login/Login.js";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-    return (
-        <div>
-            <Router>
-
-                <Switch>
-                    <Route path="/">
-                        <Main />
-                    </Route>
-                    <Route path="/register">
-                    <Navbar/>
-                    
-                    </Route>
-                    <Route path="/login">
-                    <Navbar/>
-                    <Login/>
-                    </Route>
-                   
-                </Switch>
-            </Router>
-        </div>
-    )
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Main />
+          </Route>
+          <Route path="/register">
+            <Navbar />
+          </Route>
+          <Route path="/login">
+            <Navbar />
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
