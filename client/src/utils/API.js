89f3486e43212865ec829cb29beb4d35f0e34600
Richard Font <rick.h.font@gmail.com>
2021-01-30 12:31:28 -0500

    ignore

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,8 +1,28 @@
 import axios from "axios";
 
+export default {
+  getEvents: () =>
+  axios ({
+    'method': "GET",
+    'baseURL': "http://api.eventful.com/rest/events/search?",
+    'headers': {
+      'event-type': "",
+      'apiKEY': "42qSvp8KVT9fbB65",
+      'consumerKEY': "c0e3539f8a4bef844701",
+      
+    },
+    'params': {
+        'search': "parameter",
+    },
+  })
+}
+
+
+var axios = require('axios');
+
 var config = {
   method: 'get',
-  url: 'http://api.eventful.com/rest/events/search?...&keywords=sports&location=San+Diego&date=Future&app_key=42qSvp8KVT9fbB65&oauth_consumer_key=c0e3539f8a4bef844701&oauth_token=42qSvp8KVT9fbB65&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1611426960&oauth_nonce=uaXHb3HwGVN&oauth_version=1.0&oauth_signature=w2avj4SksQUvdL2xhz2Ek0DrPlo=',
+  url: 'http://api.eventful.com/rest/events/search?...&keywords=comedy&location=Cleveland&date=Future&app_key=42qSvp8KVT9fbB65&oauth_consumer_key=c0e3539f8a4bef844701&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1612015547&oauth_nonce=T5JBIcFGLi1&oauth_version=1.0&oauth_signature=kb12yIQ8RxlArCxdh2cbc5TC2EQ=',
   headers: { }
 };
 
@@ -13,9 +33,3 @@ axios(config)
 .catch(function (error) {
   console.log(error);
 });
-
-export default {
-    getEvents: function () {
-        return axios(config)
-    }
-}
