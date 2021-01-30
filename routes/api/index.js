2103aad6c2e8b9cf33925d49fd49331051b60492
Andrew Novak <andrew.n.novak@gmail.com>
2021-01-30 10:13:39 -0500

    Updated routes/api/index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
-const orm = require("../../controllers/controller")
+const orm = require("../../controllers/controller");
+const users = require("./routes/users");
 
 router.post("/route", function (req, res) {
-    console.log(req.body)
-    res.json(req.body)
-})
-router.route("/newplace").post(orm.create)
+  console.log(req.body);
+  res.json(req.body);
+});
+router.route("/newplace").post(orm.create);
 
 module.exports = router;
