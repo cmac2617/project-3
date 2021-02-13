7f8d4cad9974a57e9d9eff693e98742d459fdc73
Cyle <cmac2617@yahoo.com>
2021-02-12 20:03:09 -0500

    change path files

--- client/src/pages/Userpage.js
+++ client/src/pages/Userpage.js
@@ -1,6 +1,6 @@
 // After user logs in.
 import UserNavbar from "../components/UserNavbar/UserNavbar.js"
-import { Input, FormBtn, StartDate, EndDate, Category, State } from "../components/NewPlaceForm/NewPlaceForm.js"
+import { Input, FormBtn, StartDate, EndDate, Category, State } from "../components/NewPlaceForm/Newplaceform.js"
 import React, { useEffect, useState } from "react";
 import API from "../utils/API";
 import "./Userpage.css";
